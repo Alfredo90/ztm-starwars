813c66aa0cd54bf63f4d86f4483e76af1ffcfa52
Alfredo <kmstry214@gmail.com>
2023-10-15 22:54:22 -0500

    want to make each div clickable

--- starwars/src/components/CardList/CardList.jsx
+++ starwars/src/components/CardList/CardList.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const CardList = ({ person: { name, height } }) => {
     return (
-        <div className=' border-2 border-black rounded-sm w-24 h-24   '>
-            <p>{name}</p>
-        </div>
+        <a href="">
+            <div className=' border-2 border-black rounded-sm w-24 h-24   '>
+                <p>{name}</p>
+            </div>
+        </a>
     )
 }
 
