fd0d62d4dd44b6dee1161f3c4c4544c9e76b2085
Alfredo <kmstry214@gmail.com>
2023-10-16 22:32:58 -0500

    update characters information

--- starwars/src/components/CardList/CardList.jsx
+++ starwars/src/components/CardList/CardList.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const CardList = ({ person: { name, height } }) => {
+const CardList = ({ person: { name, height, hair_color, eye_color, gender } }) => {
     return (
-        <a href="">
-            <div className=' border-2 border-black rounded-sm w-24 h-24   '>
-                <p>{name}</p>
-            </div>
-        </a>
+        <div className='border-2 border-black rounded-sm w-48 h-44'>
+            <p>Name: {name}</p>
+            <p>Height: {height}</p>
+            <p>Hair Color: {hair_color}</p>
+            <p>Eye Color: {eye_color}</p>
+            <p>Gender: {gender}</p>
+
+
+        </div>
     )
 }
 
